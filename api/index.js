6a6933457d4a57b95bb4bd6187683c2aa176203a
Adham Farrag <adham.farrag@hotmail.com>
2021-09-13 10:46:37 +0800

    Headers

--- api/index.js
+++ api/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
 let highlights = [{ IdOne: 'TEXT_CONTECT' }]
-
+const corsOptions = {
+  origin: 'https://nuxthighlighter.vercel.app/',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 app.use(express.json())
 app.all('/highlights', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   res.json({ highlights: highlights })
 })
 
 app.post('/highlight', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
   highlights.push(req.body)
   if (!highlights) {
     res.status(404).end('Highlight do not exsist')
